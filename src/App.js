0d31d100efb196e22931541757325448e05686b2
anderson avendaño <andersonaven7@example.com>
2023-06-27 18:48:47 -0600

    primer commit de gitignore

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+      <CreateTodoButton />
+
+
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>v</span>
+      <p>Llorar con la llorona</p>
+      <span>x</span>
+    </li>
   );
 }
 
